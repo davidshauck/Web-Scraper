94586f1a8ecb9c4ea5a51b125e888ebea71b64ee
David Hauck <dhauck@mac.com>
2020-02-05 00:58:00 -0500

    trying to get it to work on heroku

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(express.static("public"));
 
 // Connect to the Mongo DB
 // mongoose.connect("mongodb://localhost/mongoScraper", { useNewUrlParser: true });
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+mongoose.connect(MONGODB_URI, { useUnifiedTopology: true });
 
 // Main route 
 app.get("/", function(req, res) {
